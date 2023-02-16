18f1d10eb782e055198a74dfa596d79b7e08402d
Bharat Soni <bharatchills@gmail.com>
2023-02-16 15:56:18 +0530

    saving feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import domtoimage from 'dom-to-image';
+
 
 export default function Home() {
   const imageMimeType = /image\/(png|jpg|jpeg)/i;
 
   const [dropped, setDropped] = useState(false);
-  const [imageSource, setImageSource] = useState(
-    "https://img.freepik.com/free-photo/abstract-flowing-neon-wave-background_53876-101942.jpg?w=740&t=st=1676517847~exp=1676518447~hmac=1cda3de0607e68001999e2696d613ed9703e1e3fb69e6f73bf15d97153f184be"
-  );
 
   const [selectedFile, setSelectedFile] = useState();
-  const [preview, setPreview] = useState('');
+  const [preview, setPreview] = useState("");
+    const container = useRef(null);
+
+    function exportToPng(dom:any) {
+  domtoimage
+    .toPng(dom)
+    .then(function (dataUrl) {
+      let a:any = document.createElement("A");
+            a.href = dataUrl;
+            a.download = `beautify.sonicodes.in ${new Date().toISOString()}.png`;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+
+    })
+    .catch(function (error) {
+      console.error("oops, something went wrong!", error);
+    });
+}
+
 
   // create a preview as a side effect, whenever selected file is changed
   useEffect(() => {
     if (!selectedFile) {
-      setPreview('');
+      setPreview("");
       return;
     }
 
@@ -37,14 +55,66 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center main h-screen w-screen">
+    <div className="flex flex-col items-center justify-center main h-screen w-screen">
       {dropped ? (
         <>
-          <div className="flex items-center justify-center flex-col rounded-lg bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 w-1/2 h-3/4 ">
+          <div ref={container} className="relative flex items-center justify-center flex-col rounded-lg bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 w-1/2 h-3/4 ">
             <div className="image">
-              <img className="rounded-2xl max-h-96 max-w-96" src={preview} alt="" />
+              <img
+                className="rounded-2xl max-h-64  max-w-md"
+                src={preview}
+                alt=""
+              />
             </div>
           </div>
+             <div onClick={() => exportToPng(container.current)}  className="m-4 px-4 py-2 text-white bg-gradient-to-br from-red-400 to-pink-600 flex items-center justify-center duration-100 cursor-pointer rounded-2xl	 text-sm font-semibold !justify-start">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 256 256"
+          className="mr-1"
+        >
+          <rect width="256" height="256" fill="none"></rect>
+          <path
+            d="M200,224H56a8,8,0,0,1-8-8V40a8,8,0,0,1,8-8h96l56,56V216A8,8,0,0,1,200,224Z"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          ></path>
+          <polyline
+            points="152 32 152 88 208 88"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          ></polyline>
+          <polyline
+            points="100 156 128 184 156 156"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          ></polyline>
+          <line
+            x1="128"
+            y1="120"
+            x2="128"
+            y2="184"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="16"
+          ></line>
+        </svg>
+        <span>Save</span>
+      </div>
         </>
       ) : (
         <div className="flex items-center justify-center ">
@@ -63,6 +133,7 @@ export default function Home() {
           </label>
         </div>
       )}
+   
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,55 @@
 // @ts-check
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-const {default: flattenColorPalette} = require('tailwindcss/lib/util/flattenColorPalette');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./{src,app}/**/*.{ts,tsx}'],
-	theme: {
-		extend: {
-			colors: {
-				blurple: '#5865F2',
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-		{
-			handler: tw => {
-				tw.matchComponents(
-					{
-						'bg-grid': value => ({
-							backgroundSize: '90px 90px',
-							backgroundImage: `
+  content: ["./{src,app}/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blurple: "#5865F2",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    {
+      handler: (tw) => {
+        tw.matchComponents(
+          {
+            "bg-grid": (value) => ({
+              backgroundSize: "90px 90px",
+              backgroundImage: `
 								linear-gradient(to right, ${value} 1px, transparent 1px),
 								linear-gradient(to bottom, ${value} 1px, transparent 1px)
 							`,
-						}),
-					},
-					{
-						values: flattenColorPalette(tw.theme('colors')),
-						type: 'color',
-					},
-				);
+            }),
+          },
+          {
+            values: flattenColorPalette(tw.theme("colors")),
+            type: "color",
+          }
+        );
 
-				tw.matchUtilities(
-					{
-						'text-glow': value => ({
-							'text-shadow': `0 0 10px ${value}, 0 0 150px ${value}`,
-						}),
-						'glow': value => ({
-							filter: `drop-shadow(0px 0px 7px ${value})`,
-						}),
-					},
-					{
-						values: flattenColorPalette(tw.theme('colors')),
-						type: 'color',
-					},
-				);
-			},
-		},
-	],
+        tw.matchUtilities(
+          {
+            "text-glow": (value) => ({
+              "text-shadow": `0 0 10px ${value}, 0 0 150px ${value}`,
+            }),
+            glow: (value) => ({
+              filter: `drop-shadow(0px 0px 7px ${value})`,
+            }),
+          },
+          {
+            values: flattenColorPalette(tw.theme("colors")),
+            type: "color",
+          }
+        );
+      },
+    },
+  ],
 };
